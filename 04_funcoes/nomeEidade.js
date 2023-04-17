a1e3f30601df11e5faae34df982ec8f134b81749
EstherFN <111723323+EstherFN@users.noreply.github.com>
2023-04-17 18:59:53 -0300

    Add files via upload

---
+++ 04_funcoes/nomeEidade.js
@@ -0,0 +1,17 @@
+
+function escrevaSeuNome(nome) {
+    return 'Meu nome é : ' + nome;
+}
+
+function vejaSeVoceMaiorDeIdade(idade) {
+    if (idade >= 18) {
+        console.log(escrevaSeuNome('Esther') + ' É maior de idade ');
+    } else {
+        console.log('Menor de idade');
+    }
+}
+vejaSeVoceMaiorDeIdade(25);
+
+
+
+
